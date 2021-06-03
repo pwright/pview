4a484fe9de418c3aa78e53c8d154c7c72152508e
Paul Wright <pwright@redhat.com>
2021-06-03 12:02:37 +0100

    update to give more feedback to user

--- index.js
+++ index.js
@@ -7,21 +7,40 @@ const { exec } = require("child_process");
 
 var myArgs = process.argv.slice(2).join(' ');
 
-console.log(myArgs);
+// console.log(myArgs);
 
 var hamlPath = __dirname + '/haml';
 var checkPath = __dirname + '/check.sh';
+var runAsciidoc = 'asciidoctor -T ' + hamlPath + ' ' + myArgs;
+
+
 exec(checkPath, (error, data, getter) => {
 	if(error){
-		//console.log("error",error.message);
+		console.log("error",error.message);
 		return;
 	}
 	if(getter){
-		console.log("data",data);
+		console.log("Running check\n",data);
 		return;
 	}
-	console.log("data",data);
+	console.log("",data);
 
 });
 
-exec(`asciidoctor -T ${hamlPath} ${myArgs}`, {});
+
+
+exec(runAsciidoc, (error, data, getter) => {
+	if(error){
+		console.log("error",error.message);
+		return;
+	}
+	if(getter){
+		console.log("g",data);
+		return;
+	}
+	console.log("Running asciidoctor",data);
+
+});
+
+console.log('NOTE: If an html file is not created, try running: ' + runAsciidoc);
+
